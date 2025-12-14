9ae5ede0e1bc95516b873d09fa2fb9b05613f62a
MD. Tanzim Rahman Udoy <udayrahman638650@gmail.com>
2025-12-14 20:03:01 +0600

    Add files via upload

---
+++ 17.without temporary variable addition and subtraction.c
@@ -0,0 +1,11 @@
+//swap c programming
+//without temporary variable addition and subtraction
+#include<stdio.h>
+int main()
+{
+    int a=5,b=10;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("a=%d,b=%d",a,b);
+}
